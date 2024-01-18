c2ff9bd3c22bfdc9b3a962376b004255ec24db31
EVIL820 <villegas.valle.emmanuel@gmail.com>
2024-01-18 14:37:29 -0600

    se agrega el codigo parcial para el login y registro con Google

--- Front-end/login/src/App.js
+++ Front-end/login/src/App.js
@@ -3,27 +3,11 @@ import './style.css';
 import axios from 'axios';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { signInWithPopup, auth, provider } from './appAuth';  // Ajustar la ruta a appAuth.js
 
 //icons
 import { IoIosMail } from "react-icons/io";
 import { RiLockPasswordFill } from "react-icons/ri";
-
-// Configuración de Firebase
-const firebaseConfig = {
-  apiKey: 'AIzaSyC9HKu9IOojoQJ1njDkM6fH798NduMY60s',
-  authDomain: 'app-autismo-a1024.firebaseapp.com',
-  projectId: 'app-autismo-a1024',
-  storageBucket: 'app-autismo-a1024.appspot.com',
-  messagingSenderId: '717885363832',
-  appId: '1:717885363832:web:76991326ba2f60e953bf8d',
-  measurementId: 'G-K64NT3743B',
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-const provider = new GoogleAuthProvider();
 
 function App() {
   const [_identifier, setIdentifier] = React.useState('');
