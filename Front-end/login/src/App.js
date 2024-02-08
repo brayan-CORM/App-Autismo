1a0b49cf7b36126febfcf44358b9a0cb823ab1d0
EVIL820 <villegas.valle.emmanuel@gmail.com>
2024-02-08 15:50:07 -0600

    Ajustes en las redicciones y en el style

--- Front-end/login/src/App.js
+++ Front-end/login/src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import axios from 'axios';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signInWithPopup, auth, provider } from './appAuth';  // Ajustar la ruta a appAuth.js
 
@@ -9,31 +8,27 @@ import { signInWithPopup, auth, provider } from './appAuth';  // Ajustar la ruta
 import { IoIosMail } from "react-icons/io";
 import { RiLockPasswordFill } from "react-icons/ri";
 
-function App() {
-  const [_identifier, setIdentifier] = React.useState('');
-  const [_password, setPassword] = React.useState('');
-  const [error, setError] = React.useState('');
+function App({ handleLogin }) {
+  const [identifier, setIdentifier] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const navigate = useNavigate();
 
   const funcion_login = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3001/api/login', {
-        _identifier,
-        _password
-      });
+      const success = await handleLogin({ _identifier: identifier, _password: password });
 
-      if (response.data.success) {
-        console.log(response.data);
+      if (success) {
         navigate("/home");
       } else {
-        console.log(response.data);
-        return alert("Correo o contraseña incorrecta");
+        alert("Correo o contraseña incorrecta");
       }
     } catch (error) {
-      setError("Correo o contraseña incorrecta");
+      console.error('Error durante el inicio de sesión:', error);
+      alert("Correo o contraseña incorrecta");
     }
-  }
+  };
 
   const loginWithGoogle = async () => {
     try {
@@ -46,28 +41,29 @@ function App() {
 
       if (response.data.success) {
         console.log(response.data);
-        alert('Sesión iniciada con exito');
+        alert('Sesión iniciada con éxito');
         navigate('/home');
       } else {
         console.log(response.data);
         alert('Error al iniciar sesión con Google');
       }
     } catch (error) {
-      setError('Error al iniciar sesión con Google');
+      console.error('Error al iniciar sesión con Google:', error);
+      alert('Error al iniciar sesión con Google');
     }
   };
-//login con facebook (falta agregar la seguridad https para que funcione el login con facebook)
+
   const facebookLogin = () => {
-    if(!window.FB) return;
+    if (!window.FB) return;
     //hacer login
-    window.FB.getLoginStatus(response =>{
-      if (response.status === "connected"){
+    window.FB.getLoginStatus(response => {
+      if (response.status === "connected") {
         //leer los datos del usuario 
         facebookLoginHandler(response);
       }
-      else{
-        // intentar iniciar sesion
-        window.FB.login(facebookLoginHandler, {scope:'public_profile,email'});
+      else {
+        // intentar iniciar sesión
+        window.FB.login(facebookLoginHandler, { scope: 'public_profile,email' });
       }
     });
   };
@@ -75,12 +71,12 @@ function App() {
   const facebookLoginHandler = (response) => {
     console.log(response);
 
-    if(response.status === "connected"){
-      //leer datos del iusuario
-      window.FB.api('/me?fields=id,name,email,picture', userData =>{
+    if (response.status === "connected") {
+      //leer datos del usuario
+      window.FB.api('/me?fields=id,name,email,picture', userData => {
         console.log(userData);
 
-        //almacenar la sesion del usuario en nuestra app
+        //almacenar la sesión del usuario en nuestra app
       });
     }
   };
@@ -91,81 +87,81 @@ function App() {
 
   function go_to_register() {
     navigate("/register");
-  } 
+  }
 
   return (
-      <div className='login'>
-        <img src='../Logo.png' width="150" height="150" />
+    <div className='login'>
+      <img src='../Logo.png' width="150" height="150" />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <form onSubmit={funcion_login}>
+        <br />
+        <IoIosMail className='icon_Mail' />
+        <input className='Correo'
+          id='user'
+          type='text'
+          value={identifier}
+          onChange={(event) => setIdentifier(event.target.value)}
+          placeholder='Correo o Usuario'
+        />
         <br />
         <br />
+
+        <RiLockPasswordFill className='icon_password' />
+        <input className='password'
+          id='Password'
+          type='password'
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          placeholder='Contraseña'
+        />
+        <br />
+
+        <div className='olive_contraseña'>
+          <p><a onClick={go_to_recoverpassword}>¿Olvidaste tu contraseña?</a></p>
+        </div>
+
+        {error && <p className='error'>{error}</p>}
+        <br />
+
+        <button className='b_login' type='submit'>Iniciar sesión</button>
         <br />
         <br />
 
-        <form onSubmit={funcion_login}>
-          <br />
-          <IoIosMail className='icon_Mail' />
-          <input className='Correo'
-            id='user'
-            type='text'
-            value={_identifier}
-            onChange={(event) => setIdentifier(event.target.value)}
-            placeholder='Correo o Usuario'
-          />
-          <br />
-          <br />
-
-          <RiLockPasswordFill className='icon_password' />
-          <input className='password'
-            id='Password'
-            type='password'
-            value={_password}
-            onChange={(event) => setPassword(event.target.value)}
-            placeholder='Contraseña'
-          />
-          <br />
-
-          <div className='olive_contraseña'>
-            <p><a onClick={go_to_recoverpassword}>¿Olvidaste tu contraseña?</a></p>
-          </div>
-
-          {error && <p className='error'>{error}</p>}
-          <br />
-
-          <button className='b_login' type='submit'>Iniciar sesión</button>
-          <br />
-          <br />
-
-          <div className='b_tipo_login'>
-            <button className='b_google' type='button' 
+        <div className='b_tipo_login'>
+          <button className='b_google' type='button'
             onClick={loginWithGoogle}>
-              <div className='l_google'>
+            <div className='l_google'>
               <p className='G'>G
-                  <span className='O'>o</span>
-                  <span className='o'>o</span>
-                  <span className='g'>g</span>
-                  <span className='l'>l</span>
-                  <span className='e'>e</span>
-                </p>
-              </div>
-            </button>
-            <button className='b_faccebook'
+                <span className='O'>o</span>
+                <span className='o'>o</span>
+                <span className='g'>g</span>
+                <span className='l'>l</span>
+                <span className='e'>e</span>
+              </p>
+            </div>
+          </button>
+          <button className='b_faccebook'
             type='submit'
             onClick={facebookLogin}
-            >facebook</button>
-          </div>
+          >facebook</button>
+        </div>
 
+        <br />
+
+        <div className='Registro'>
+          <p>¿No tienes cuenta?
+            <u>
+              <a id='registro' onClick={go_to_register}> Regístrate</a>
+            </u>
+          </p>
           <br />
-
-          <div className='Registro'>
-            <p>¿No tienes cuenta?
-              <u>
-                <a id='registro' onClick={go_to_register}> Regístrate</a>
-              </u>
-            </p>
-            <br />
-          </div>
-        </form>
-      </div>
+        </div>
+      </form>
+    </div>
   );
 }
 
--- Front-end/login/src/AppRouter.js
+++ Front-end/login/src/AppRouter.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import App from "./App";
 import Home from "./Pages/Home";
 import Register from "./Pages/Register";
@@ -12,24 +12,53 @@ import People from "./Pages/people";
 import ResetPassword from "./Pages/resetpassword";
 import Calendar from "./Pages/calendar";
 
-const AppRouter = ()=>{
-    return(
-        <Router>
-            <Routes>
-                <Route path="/" element={<App />}/>
-                <Route path="/home" element={<Home/>}/> 
-                <Route path="/register" element={<Register/>}/>
-                <Route path="/recover-password" element={<RecoverPassword/>}/>
-                <Route path="/actions" element={<Actions/>}/>
-                <Route path="/food" element={<Food/>}/>
-                <Route path="/feelings" element={<Feelings/>}/>
-                <Route path="/hygiene" element={<Hygiene/>}/>
-                <Route path="/people" element={<People/>}/>
-                <Route path="/reset-password/:resetId" element={<ResetPassword/>}/>
-                <Route path="/calendar" element={<Calendar/>}/>
-            </Routes>
-        </Router>
-    );
-}
+const AppRouter = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = async ({ _identifier, _password }) => {
+    try {
+      // Realizar una solicitud al servidor backend para iniciar sesión
+      const response = await fetch('http://localhost:3001/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ _identifier, _password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // Si la respuesta es exitosa, establecer isAuthenticated a true
+        setIsAuthenticated(true);
+        return true;
+      } else {
+        console.log(data.msg); // Manejar mensaje de error, por ejemplo, mostrar un mensaje al usuario
+        return false;
+      }
+    } catch (error) {
+      console.error('Error durante el inicio de sesión:', error);
+      return false;
+    }
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App handleLogin={handleLogin} />} />
+        <Route path="/actions" element={isAuthenticated ? <Actions /> : <Navigate to="/" />} />
+        <Route path="/calendar" element={isAuthenticated ? <Calendar /> : <Navigate to="/" />} />
+        <Route path="/feelings" element={isAuthenticated ? <Feelings /> : <Navigate to="/" />} />
+        <Route path="/food" element={isAuthenticated ? <Food /> : <Navigate to="/" />} />
+        <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/" />} />
+        <Route path="/hygiene" element={isAuthenticated ? <Hygiene /> : <Navigate to="/" />} />
+        <Route path="/people" element={isAuthenticated ? <People /> : <Navigate to="/" />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/recover-password" element={<RecoverPassword />} />
+        <Route path="/reset-password/:resetId" element={<ResetPassword />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default AppRouter;
