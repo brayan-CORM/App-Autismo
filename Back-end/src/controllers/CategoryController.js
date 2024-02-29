851f32edd36df7f5f4ee070b9cfa0c0071d7a5fa
EVIL820 <villegas.valle.emmanuel@gmail.com>
2024-02-28 23:03:00 -0600

    Ajustes menores

--- Back-end/src/controllers/CategoryController.js
+++ Back-end/src/controllers/CategoryController.js
@@ -6,40 +6,42 @@ const CategoryService = require("../services/CategoryService");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
-router.post("/upload", upload.single("categoryImage"), async (req, res) => {
+// Ruta para agregar una o varias categorías
+router.post("/upload", upload.array("categoryImages"), async (req, res) => {
   try {
-    const { categoryName } = req.body;
-    const originalImageName = req.file.originalname; // Obtener el nombre original de la imagen
+    const { categoryNames } = req.body;
+    const categoryImages = req.files; // Obtener la lista de imágenes de categorías
 
-    // Llama al servicio para crear la categoría
-    const newCategory = await CategoryService.createCategory(
-      categoryName,
-      req.file // Pasa el objeto de la imagen completo
+    // Llama al servicio para crear las categorías
+    const newCategories = await CategoryService.createCategories(
+      categoryNames,
+      categoryImages // Pasa la lista de objetos de imágenes completos
     );
 
-    res.status(201).json(newCategory);
+    res.status(201).json(newCategories);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
+// Ruta para agregar una o varias pictogramas a una categoría específica
 router.post(
   "/uploadPictograms",
-  upload.single("pictogramImage"),
+  upload.array("pictogramImages"),
   async (req, res) => {
     try {
-      const { categoryId, pictogramName } = req.body;
-      const originalImageName = req.file.originalname; // Obtener el nombre original de la imagen
+      const { categoryId, pictogramNames } = req.body;
+      const pictogramImages = req.files; // Obtener la lista de imágenes de pictogramas
 
-      // Llama al servicio para agregar un pictograma a la categoría
-      const newPictogram = await CategoryService.addPictogram(
+      // Llama al servicio para agregar los pictogramas a la categoría
+      const newPictograms = await CategoryService.addPictograms(
         categoryId,
-        pictogramName,
-        req.file // Pasa el objeto de la imagen completo
+        pictogramNames,
+        pictogramImages // Pasa la lista de objetos de imágenes completos
       );
 
-      res.status(201).json(newPictogram);
+      res.status(201).json(newPictograms);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Internal Server Error" });
--- Back-end/src/services/CategoryService.js
+++ Back-end/src/services/CategoryService.js
@@ -3,51 +3,68 @@ const fs = require("fs");
 const path = require("path");
 
 const CategoryService = {
-  createCategory: async function (categoryName, categoryImage) {
+  createCategories: async function (categoryNames, categoryImages) {
     try {
-      // Obtén el nombre original del archivo desde la ruta proporcionada por Multer
-      const originalFileName = categoryImage.originalname;
+      const newCategories = [];
 
-      // Mueve la imagen al directorio de uploads y usa su nombre original
-      const newPath = path.join(__dirname, "..", "uploads", originalFileName);
-      fs.renameSync(categoryImage.path, newPath);
+      for (let i = 0; i < categoryNames.length; i++) {
+        const categoryName = categoryNames[i];
+        const categoryImage = categoryImages[i];
 
-      // Crea una nueva instancia de la categoría con los datos proporcionados
-      const newCategory = new Category({
-        categoryName: categoryName,
-        categoryImage: originalFileName, // Utiliza el nombre original de la imagen
-      });
+        // Obtén el nombre original del archivo desde la ruta proporcionada por Multer
+        const originalFileName = categoryImage.originalname;
 
-      // Guarda el nombre de la categoría y el nombre de la imagen en la base de datos
-      await newCategory.save();
+        // Mueve la imagen al directorio de uploads y usa su nombre original
+        const newPath = path.join(__dirname, "..", "uploads", originalFileName);
+        fs.renameSync(categoryImage.path, newPath);
 
-      return newCategory;
+        // Crea una nueva instancia de la categoría con los datos proporcionados
+        const newCategory = new Category({
+          categoryName: categoryName,
+          categoryImage: originalFileName, // Utiliza el nombre original de la imagen
+        });
+
+        // Guarda el nombre de la categoría y el nombre de la imagen en la base de datos
+        await newCategory.save();
+
+        newCategories.push(newCategory);
+      }
+
+      return newCategories;
     } catch (error) {
       throw error;
     }
   },
 
-  addPictogram: async function (categoryId, pictogramName, pictogramImage) {
+  addPictograms: async function (categoryId, pictogramNames, pictogramImages) {
     try {
       // Obtén la categoría por su ID
       const category = await Category.findById(categoryId);
 
-      // Mueve la imagen al directorio de uploads y usa su nombre original
-      const originalFileName = pictogramImage.originalname;
-      const newPath = path.join(__dirname, "..", "uploads", originalFileName);
-      fs.renameSync(pictogramImage.path, newPath);
+      const newPictograms = [];
 
-      // Agrega el nuevo pictograma a la lista de pictogramas de la categoría
-      category.pictograms.push({
-        pictogramName: pictogramName,
-        pictogramImage: originalFileName, // Utiliza el nombre original de la imagen
-      });
+      for (let i = 0; i < pictogramNames.length; i++) {
+        const pictogramName = pictogramNames[i];
+        const pictogramImage = pictogramImages[i];
+
+        // Mueve la imagen al directorio de uploads y usa su nombre original
+        const originalFileName = pictogramImage.originalname;
+        const newPath = path.join(__dirname, "..", "uploads", originalFileName);
+        fs.renameSync(pictogramImage.path, newPath);
+
+        // Agrega el nuevo pictograma a la lista de pictogramas de la categoría
+        category.pictograms.push({
+          pictogramName: pictogramName,
+          pictogramImage: originalFileName, // Utiliza el nombre original de la imagen
+        });
+
+        newPictograms.push(category.pictograms[category.pictograms.length - 1]);
+      }
 
       // Guarda los cambios en la categoría
       await category.save();
 
-      // Retorna el nuevo pictograma añadido
-      return category.pictograms[category.pictograms.length - 1];
+      return newPictograms;
     } catch (error) {
       throw error;
     }
